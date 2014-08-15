f6bb5507e4ccf7a9b13d8661e08d51e372081ed9
blacklion11 <mellodogg101@gmail.com>
2014-08-14 23:04:33 -0500

    world gen update

--- src/world/gen/WorldGenerator.java
+++ src/world/gen/WorldGenerator.java
@@ -32,7 +32,31 @@ public class WorldGenerator
 		
 		String seed = generateSeed();
 		
-		ArrayList<Integer> mapList = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> mapList = new ArrayList<ArrayList<Integer>>();
+		mapList.add(new ArrayList<Integer>());
+		mapList.add(new ArrayList<Integer>());
+		
+		mapList.get(0).add(new Integer(Integer.parseInt(seed.substring(0,2))));
+		mapList.get(0).add(new Integer(Integer.parseInt(seed.substring(2,4))));
+		mapList.get(1).add(new Integer(Integer.parseInt(seed.substring(4,6))));
+		mapList.get(1).add(new Integer(Integer.parseInt(seed.substring(6,8))));
+		
+		mapList = diamondSquare(mapList);
+		
+		for(int i = 0; i < mapList.size(); i++)
+		{
+			System.out.print(mapList.get(0).get(i) + ", ");
+		}
+		System.out.println();
+		
+		mapList = diamondSquare(mapList);
+		
+		for(int i = 0; i < mapList.size(); i++)
+		{
+			System.out.print(mapList.get(0).get(i) + ", ");
+		}
+		
+		
 		
 		return null;
 	}
@@ -42,20 +66,35 @@ public class WorldGenerator
 		Random r = new Random();
 	
 		//set the four corner seeds for the map
-		String seed = "" + r.nextInt(100);
-		seed += r.nextInt(100);
-		seed += r.nextInt(100);
-		seed += r.nextInt(100);
+		String seed = "";
+		for(int i = 0; i < 4; i++)
+		{
+			int temp = r.nextInt(100);
+			if(temp < 10)
+				seed += "0" + temp;
+			else 
+				seed += temp;
+		}
 		
-		System.out.println(seed);
+		System.out.println("Seed: " + seed);
 		
 		return seed;
 	}
 
-	private ArrayList<Integer> diamondSquare(ArrayList<Integer> map)
+	private ArrayList<ArrayList<Integer>> diamondSquare(ArrayList<ArrayList<Integer>> map)
 	{
+		int lengthX = map.size();
+		int lengthY = map.get(0).size();
+		
+		for(int i = 0; i < lengthX - 1; i++)
+		{
+			for(int j = 0; j < lengthY - 1; j++)
+			{
+				map.get(i).add(i + 1, (map.get(i).get(j).intValue() + map.get(i + 1).get(j).intValue()) / 2);
+			}
+		}
 	
-		return null;
+		return map;
 	}
 	
 	
